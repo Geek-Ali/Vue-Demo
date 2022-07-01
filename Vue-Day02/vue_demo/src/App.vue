8cc7e952382f71d422863be51f3f7beca35e3c41
Geek-Ali <alcode@qq.com>
2022-07-01 10:37:49 +0800

    Hello
    Signed-off-by: Geek-Ali <alcode@qq.com>

--- Vue-Day02/vue_demo/src/App.vue
+++ Vue-Day02/vue_demo/src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <h1>Vue组件基础！</h1>
+  <h3>{{UserName}}</h3>
+  <hr>
+  <p>Count的值：{{count}}</p>
+  <button class="buttonStyle" @click="addCount">+1</button>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default{
+  name:'MyApp',
+  data(){
+    return{
+      UserName:'Hello Vue!',
+      count:0
+    }
+  },
+  methods:{
+    addCount(){
+      this.count++
+    }
   }
 }
 </script>
+<style>
+  .buttonStyle{
+    width: 100%;
+    height: 50px;
+    border-radius: 30px;
+    background-color: rgb(129, 129, 211);
+  }
+</style>
--- Vue-Day02/vue_demo/src/main.js
+++ Vue-Day02/vue_demo/src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
+//导入createApp函数
+import { createApp } from "vue";
+//导入待渲染App.vue组件
 import App from './App.vue'
-import './index.css'
-
-createApp(App).mount('#app')
+//调用createApp函数 ， 创建SPA应用实例
+const app=createApp(App)
+app.mount('#app')
